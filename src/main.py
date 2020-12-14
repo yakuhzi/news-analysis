cf973b6858f42e2cc1466216ea580e9fed086545
Kim-Celine Kahl <kim-celine.kahl@web.de>
2020-12-14 12:43:43 +0100

    read data into pandas dataframes

--- src/main.py
+++ src/main.py
@@ -4,6 +4,13 @@ if __name__ == "__main__":
     reader = Reader()
     reader.read_articles()
 
-    print("Number of Bild articles: {}".format(len(reader.bild_articles)))
-    print("Number of Tagesschau articles: {}".format(len(reader.tagesschau_articles)))
-    print("Number of TAZ articles: {}".format(len(reader.taz_articles)))
+    print("Number of Bild articles: {}".format(len(reader.df_bild_articles.index)))
+    print(
+        "Number of Tagesschau articles: {}".format(
+            len(reader.df_tagesschau_articles.index)
+        )
+    )
+    print("Number of TAZ articles: {}".format(len(reader.df_taz_articles.index)))
+
+    print(reader.df_tagesschau_articles.dtypes)
+    print(reader.df_tagesschau_articles.head())
--- src/utils/reader.py
+++ src/utils/reader.py
@@ -1,41 +1,54 @@
 import json
 from typing import List, Optional
 
+import pandas as pd
+
 
 class Reader:
     """
     Class that reads the news articles from the json files.
 
     Attributes:
-    - bild_articles: A list of all BILD news articles.
-    - tagesschau_articles: A list of all Tagesschau news articles.
-    - taz_articles: A list of all TAZ news articles.
+    - df_bild_articles: A pandas dataframe of all BILD news articles.
+    - df_tagesschau_articles: A pandas dataframe of all Tagesschau news articles.
+    - df_taz_articles: A pandas dataframe of all TAZ news articles.
     """
 
     def __init__(self):
-        self.bild_articles: Optional[List[dict]] = None
-        self.tagesschau_articles: Optional[List[dict]] = None
-        self.taz_articles: Optional[List[dict]] = None
+        self.df_bild_articles: Optional[List[dict]] = None
+        self.df_tagesschau_articles: Optional[List[dict]] = None
+        self.df_taz_articles: Optional[List[dict]] = None
 
     def read_articles(self) -> None:
         """
         Reads the news article for every news agency and stores it in the corresponding instance variables.
         """
 
-        self.bild_articles = self.__read("src/data/bild.json")
-        self.tagesschau_articles = self.__read("src/data/tagesschau.json")
-        self.taz_articles = self.__read("src/data/taz.json")
+        self.df_bild_articles = self.__read("src/data/bild.json")
+        self.df_tagesschau_articles = self.__read("src/data/tagesschau.json")
+        self.df_taz_articles = self.__read("src/data/taz.json")
 
-    def __read(self, path: str) -> List[dict]:
+    @staticmethod
+    def __read(path: str) -> pd.DataFrame:
         """
-        Helper function to read a json from a file.
+        Helper function to read a json from a file and store it in pandas dataframe.
 
         Arguments:
         - path: Path to json file.
 
         Return:
-        - articles: List of JSON articles parsed from the input file.
+        - articles: Panda data frame of JSON articles parsed from the input file.
         """
 
         with open(path, encoding="utf8") as json_file:
-            return json.load(json_file)["articles"]
+            json_dict = json.load(json_file)["articles"]
+            return pd.DataFrame(json_dict).astype(
+                {
+                    "title": "string",
+                    "text": "string",
+                    "summary": "string",
+                    "date": "string",
+                    "authors": "object",
+                    "references": "object",
+                }
+            )
