51cf32aa2ef379ad6ff8422760f79cca05aa10fe
SophiaMat <sophiamatthis@outlook.de>
2020-12-15 20:10:10 +0100

    #2 lowercasing and removing special characters

--- src/preprocessing_Articles.py
+++ src/preprocessing_Articles.py
@@ -3,11 +3,19 @@ import pandas as pd
 
 class Preprocess_Articles:
     def preprocessing(self, articles: pd.DataFrame):
-        preprocessed_articles = articles.copy()
-        preprocessed_articles = preprocessed_articles[:10]
-        print(preprocessed_articles)
+        df_preprocessed_articles = articles.copy()
+        df_preprocessed_articles = df_preprocessed_articles[:10]
+
+        """ lowercase everything"""
+        df_preprocessed_articles["text"] = df_preprocessed_articles["text"].str.lower()
+        print(df_preprocessed_articles["text"])
         """remove special characters (regex)"""
+        df_preprocessed_articles["text"] = df_preprocessed_articles["text"].str.replace(
+            r"[^a-z0-9äöü ]", ""
+        )
+
         """stop word removal (after POS? -> filter unwanted POS)"""
+
         """ tokenization """
         """ POS tagging (before stemming? Could be used to count positive or negative adjectives etc. """
         """ stemming or lemmatization"""
