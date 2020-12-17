8cf1b0ca9618b3bdae21f4c4002388b97b07b28a
Kim-Celine Kahl <kim-celine.kahl@web.de>
2020-12-17 17:53:06 +0100

    Do NER tagging within preprocessing pipeline
    Co-authored-by: SophiaMat <sophiamatthis@outlook.de>

--- src/main.py
+++ src/main.py
@@ -2,46 +2,46 @@ from pathlib import Path
 
 import pandas as pd
 
-import src.preprocessing_articles
+from src.preprocessing_articles import PreprocessArticles
 from src.utils.reader import Reader
 from src.utils.writer import Writer
 
-
-def get_ner_df(ner_json_file: str, df_to_tag: pd.DataFrame) -> pd.DataFrame:
-    """
-    Helper function to get the ner tagged pandas dataframe. If the tagging already was done ones (JSON files with
-    tags exist) the tagging is not done again but the json files with the tags are read into a pandas data frame.
-    If ner tagging is proceeded, the result will be stored in a json file.
-
-    Arguments:
-    - ner_json_file: Name of json file to store/ read the results of ner tagging.
-    - df_to_tag: data frame with the text to tag, if the data still needs to be tagged
-
-    Return:
-    - df_ner: Pandas data frame of the ner tagged input
-    """
-    ner_json_path = "src/data/" + ner_json_file + ".json"
-    if not Path(ner_json_path).exists():
-        df_ner = apply_ner_tagging(df_to_tag)
-        writer.write_articles(df_ner, ner_json_file)
-    else:
-        df_ner = reader.read_json_to_df_default(ner_json_path)
-    return df_ner
-
-
-def apply_ner_tagging(df_to_tag: pd.DataFrame) -> pd.DataFrame:
-    """
-    Apply the ner tagging on a given data frame
-
-    Arguments:
-    - df_to_tag: the Pandas data frame which should be tagged
-
-    Return:
-    - df_ner: Pandas data frame of the ner tagged input
-    """
-    df_ner = df_to_tag.apply(src.preprocessing_articles.tag_dataframe, axis=1)
-    df_ner = df_ner.filter(["persons", "organizations"], axis=1)
-    return df_ner
+# def get_ner_df(ner_json_file: str, df_to_tag: pd.DataFrame) -> pd.DataFrame:
+#     """
+#     Helper function to get the ner tagged pandas dataframe. If the tagging already was done ones (JSON files with
+#     tags exist) the tagging is not done again but the json files with the tags are read into a pandas data frame.
+#     If ner tagging is proceeded, the result will be stored in a json file.
+#
+#     Arguments:
+#     - ner_json_file: Name of json file to store/ read the results of ner tagging.
+#     - df_to_tag: data frame with the text to tag, if the data still needs to be tagged
+#
+#     Return:
+#     - df_ner: Pandas data frame of the ner tagged input
+#     """
+#     ner_json_path = "src/data/" + ner_json_file + ".json"
+#     if not Path(ner_json_path).exists():
+#         df_ner = apply_ner_tagging(df_to_tag)
+#         writer.write_articles(df_ner, ner_json_file)
+#     else:
+#         df_ner = reader.read_json_to_df_default(ner_json_path)
+#     return df_ner
+#
+#
+# def apply_ner_tagging(df_to_tag: pd.DataFrame) -> pd.DataFrame:
+#     """
+#     Apply the ner tagging on a given data frame
+#
+#     Arguments:
+#     - df_to_tag: the Pandas data frame which should be tagged
+#
+#     Return:
+#     - df_ner: Pandas data frame of the ner tagged input
+#     """
+#     preprocess = PreprocessArticles()
+#     df_ner = df_to_tag.apply(preprocess.tag_dataframe, axis=1)
+#     df_ner = df_ner.filter(["persons", "organizations"], axis=1)
+#     return df_ner
 
 
 if __name__ == "__main__":
@@ -59,25 +59,25 @@ if __name__ == "__main__":
     print(reader.df_tagesschau_articles.dtypes)
     print(reader.df_tagesschau_articles.head())
 
-    preprocess = src.preprocessing_articles.PreprocessArticles()
+    preprocess = PreprocessArticles()
     bild = preprocess.preprocessing(reader.df_bild_articles)
     writer = Writer()
     writer.write_articles(bild, "bild_preprocessed")
 
-    bild_ner_file = "bild_ner"
-    df_bild_ner = get_ner_df(bild_ner_file, reader.df_bild_articles)
-
-    print(df_bild_ner.dtypes)
-    print(df_bild_ner.head())
-
-    tagesschau_ner_file = "tagesschau_ner"
-    df_tagesschau_ner = get_ner_df(tagesschau_ner_file, reader.df_tagesschau_articles)
-
-    print(df_tagesschau_ner.dtypes)
-    print(df_tagesschau_ner.head())
-
-    taz_ner_file = "taz_ner"
-    df_taz_ner = get_ner_df(taz_ner_file, reader.df_taz_articles)
-
-    print(df_taz_ner.dtypes)
-    print(df_taz_ner.head())
+    # bild_ner_file = "bild_ner"
+    # df_bild_ner = get_ner_df(bild_ner_file, reader.df_bild_articles)
+    #
+    # print(df_bild_ner.dtypes)
+    # print(df_bild_ner.head())
+    #
+    # tagesschau_ner_file = "tagesschau_ner"
+    # df_tagesschau_ner = get_ner_df(tagesschau_ner_file, reader.df_tagesschau_articles)
+    #
+    # print(df_tagesschau_ner.dtypes)
+    # print(df_tagesschau_ner.head())
+    #
+    # taz_ner_file = "taz_ner"
+    # df_taz_ner = get_ner_df(taz_ner_file, reader.df_taz_articles)
+    #
+    # print(df_taz_ner.dtypes)
+    # print(df_taz_ner.head())
--- src/preprocessing_Articles.py
+++ src/preprocessing_Articles.py
@@ -5,104 +5,115 @@ import spacy
 from spacy.lang.de.stop_words import STOP_WORDS
 
 
-def lowercase_article(articles):
-    articles["text"] = articles["text"].str.lower()
-
-
-def remove_special_characters(df_preprocessed_articles):
-    df_preprocessed_articles["text"] = df_preprocessed_articles["text"].str.replace(
-        r"[^a-z0-9äöü ]", ""
-    )
-
-
-def remove_stopwords(df_preprocessed_articles):
-    stopwords = spacy.lang.de.stop_words.STOP_WORDS
-    df_preprocessed_articles["text"] = df_preprocessed_articles["text"].apply(
-        lambda words: " ".join(word for word in words.split() if word not in stopwords)
-    )
-
-
-def tokenization(df_preprocessed_articles, nlp):
-    df_preprocessed_articles["text"] = df_preprocessed_articles["text"].apply(
-        lambda x: nlp(x)
-    )
-
-
-def pos_tagging(df_preprocessed_articles):
-    df_preprocessed_articles["pos_tags"] = df_preprocessed_articles["text"].apply(
-        lambda row: [(word, word.tag_) for word in row]
-    )
-
-
-def lemmatizing(df_preprocessed_articles):
-    df_preprocessed_articles["lemma"] = df_preprocessed_articles["text"].apply(
-        lambda row: [word.lemma_ for word in row]
-    )
-
-
-def tag_dataframe(row: pd.Series) -> pd.Series:
-    """
-    Function to apply on Pandas data frame that it is tagged
-
-    Arguments:
-    - row: the current row of the data frame to be tagged
-
-    Return:
-    - row: Pandas series with the tagged text in colums 'persons' and 'rows'
-    """
-    persons, organizations = tag(row.text)
-    row["persons"] = persons
-    row["organizations"] = organizations
-    return row
-
-
-def tag(content: str) -> Tuple[List[str], List[str]]:
-    """
-    Searches for Names and Organizations in texts in order to identify relevant articles with political parties
-
-    Arguments:
-    - content: The text to search for the Named Entities
-
-    Return:
-    - person_list: List of recognized persons in the text.
-    - organization_list: List of organizations in the text.
-    """
-    #  de_core_news_lg had the best score for entity recognition in german according to spacy.
-    #  for more information, see https://spacy.io/models/de#de_core_news_lg
-    nlp = spacy.load("de_core_news_lg", disable=["tagger", "parser"])
-    doc = nlp(content)
-    #  search for persons and apply filter that only persons remain in list
-    filtered_persons = filter(lambda entity: entity.label_ == "PER", doc.ents)
-    person_list = list(map(lambda entity: entity.text, filtered_persons))
-    #  search for organizations and apply filter that only persons remain in list
-    filtered_organizations = filter(lambda entity: entity.label_ == "ORG", doc.ents)
-    organization_list = list(map(lambda entity: entity.text, filtered_organizations))
-    return person_list, organization_list
-
-
 class PreprocessArticles:
+    def __init__(self):
+        self.nlp = None
+
+    def lowercase_article(self, articles):
+        articles["text"] = articles["text"].str.lower()
+
+    def replace_new_line(self, df_preprocessed_articles):
+        df_preprocessed_articles["text"] = df_preprocessed_articles["text"].str.replace(
+            "\\n", " "
+        )
+
+    def remove_special_characters(self, df_preprocessed_articles):
+        df_preprocessed_articles["text"] = df_preprocessed_articles["text"].str.replace(
+            r"[^A-Za-z0-9äöüÄÖÜ\- ]", ""
+        )
+
+    def remove_stopwords(self, df_preprocessed_articles):
+        stopwords = spacy.lang.de.stop_words.STOP_WORDS
+        df_preprocessed_articles["text"] = df_preprocessed_articles["text"].apply(
+            lambda words: " ".join(
+                word for word in words.split() if word not in stopwords
+            )
+        )
+
+    def tokenization(self, df_preprocessed_articles):
+        df_preprocessed_articles["text"] = df_preprocessed_articles["text"].apply(
+            lambda x: self.nlp(x)
+        )
+
+    def pos_tagging(self, df_preprocessed_articles):
+        df_preprocessed_articles["pos_tags"] = df_preprocessed_articles["text"].apply(
+            lambda row: [(word, word.tag_) for word in row]
+        )
+
+    def lemmatizing(self, df_preprocessed_articles):
+        df_preprocessed_articles["lemma"] = df_preprocessed_articles["text"].apply(
+            lambda row: [word.lemma_ for word in row]
+        )
+
+    def tag_dataframe(self, row: pd.Series) -> pd.Series:
+        """
+        Function to apply on Pandas data frame that it is tagged
+
+        Arguments:
+        - row: the current row of the data frame to be tagged
+
+        Return:
+        - row: Pandas series with the tagged text in colums 'persons' and 'rows'
+        """
+        persons, organizations = self.tag(row.text)
+        row["persons"] = persons
+        row["organizations"] = organizations
+        return row
+
+    def tag(self, content: str) -> Tuple[List[str], List[str]]:
+        """
+        Searches for Names and Organizations in texts in order to identify relevant articles with political parties
+
+        Arguments:
+        - content: The text to search for the Named Entities
+
+        Return:
+        - person_list: List of recognized persons in the text.
+        - organization_list: List of organizations in the text.
+        """
+        doc = self.nlp(content)
+        #  search for persons and apply filter that only persons remain in list
+        filtered_persons = filter(lambda entity: entity.label_ == "PER", doc.ents)
+        person_list = list(map(lambda entity: entity.text, filtered_persons))
+        #  search for organizations and apply filter that only persons remain in list
+        filtered_organizations = filter(lambda entity: entity.label_ == "ORG", doc.ents)
+        organization_list = list(
+            map(lambda entity: entity.text, filtered_organizations)
+        )
+        return person_list, organization_list
+
     def preprocessing(self, articles: pd.DataFrame):
         df_preprocessed_articles = articles.copy()
         df_preprocessed_articles = df_preprocessed_articles[:10]
 
-        """ lowercase everything"""
-        lowercase_article(df_preprocessed_articles)
+        # lowercase everything
+        self.lowercase_article(df_preprocessed_articles)
+        self.replace_new_line(df_preprocessed_articles)
 
-        """remove special characters (regex)"""
-        remove_special_characters(df_preprocessed_articles)
+        # remove special characters (regex)
+        self.remove_special_characters(df_preprocessed_articles)
 
-        """stop word removal (after POS? -> filter unwanted POS)"""
+        # de_core_news_lg had the best score for entity recognition and syntax accuracy in german according to spacy.
+        # for more information, see https://spacy.io/models/de#de_core_news_lg
+        self.nlp = spacy.load("de_core_news_lg", disable=["parser"])
+
+        # NER Tagging for persons and organizations
+        df_preprocessed_articles = df_preprocessed_articles.apply(
+            self.tag_dataframe, axis=1
+        )
+
+        # stop word removal (after POS? -> filter unwanted POS)
         # evtl verbessern (opel fährt auf transporter auf --> opel fährt transporter)
-        nlp = spacy.load("de_core_news_lg", disable=["parser", "ner"])
-        remove_stopwords(df_preprocessed_articles)
 
-        """ tokenization """
-        tokenization(df_preprocessed_articles, nlp)
+        self.remove_stopwords(df_preprocessed_articles)
 
-        """ POS tagging (before stemming? Could be used to count positive or negative adjectives etc. """
-        pos_tagging(df_preprocessed_articles)
+        # tokenization
+        self.tokenization(df_preprocessed_articles)
 
-        """ stemming or lemmatization"""
-        lemmatizing(df_preprocessed_articles)
+        # POS tagging (before stemming? Could be used to count positive or negative adjectives etc.
+        self.pos_tagging(df_preprocessed_articles)
+
+        # stemming or lemmatization
+        self.lemmatizing(df_preprocessed_articles)
 
         return df_preprocessed_articles
