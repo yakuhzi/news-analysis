595b4e049925da7184d40e24045682ae2e36969b
SophiaMat <sophiamatthis@outlook.de>
2021-02-03 11:00:59 +0100

    remove direct quotations

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -142,6 +142,9 @@ class Preprocessing:
         # Copy original dataframe
         df_preprocessed = dataframe.copy()
 
+        # remove direct quotiations
+        df_preprocessed["text"] = self._remove_direct_quotations(df_preprocessed["text"])
+
         # Remove special characters
         df_preprocessed["text"] = self._remove_special_characters(df_preprocessed["text"])
 
@@ -200,6 +203,11 @@ class Preprocessing:
             .str.replace(r" - ", " ", regex=False)
             .str.replace(r" +", " ", regex=True)
             .str.strip()
+        )
+
+    def _remove_direct_quotations(self, text_series: Series) -> Series:
+        return (
+            text_series.str.replace(r'"(.*?)"', " ", regex=True)
         )
 
     def _remove_stopwords(self, text_series: Series) -> Series:
