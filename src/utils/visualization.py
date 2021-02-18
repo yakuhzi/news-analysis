5d04c8a3f8351eae0519c577b89e5d7307fd1bdf
SophiaMat <sophiamatthis@outlook.de>
2021-02-18 09:56:46 +0100

    shows plots for each term in gui, media not properly shown

--- src/utils/visualization.py
+++ src/utils/visualization.py
@@ -4,6 +4,7 @@ from typing import Dict, Tuple
 import datetime
 import matplotlib.pyplot as plt
 from pandas import DataFrame
+import numpy as np
 
 
 class Visualization:
@@ -19,7 +20,6 @@ class Visualization:
         figures = []
 
         for key_1, value_1 in statistics.items():
-            lines = []
 
             if by_party:
                 # Group pie charts by party
@@ -50,8 +50,7 @@ class Visualization:
             for (key_2, value_2), ax in zip(value_1.items(), axs):
                 ax.axis("equal")
                 ax.set_title(key_2)
-                line = ax.pie(value_2, colors=colors, counterclock=False, autopct="%1.1f%%", shadow=True, startangle=90)
-                lines.append(line)
+                ax.pie(value_2, colors=colors, counterclock=False, autopct="%1.1f%%", shadow=True, startangle=90)
 
             # Add legend to plot
             fig.legend(labels=labels, loc="lower right", borderaxespad=0.1, title="Sentiment")
@@ -61,8 +60,31 @@ class Visualization:
     @staticmethod
     def get_plots(df_time_course: DataFrame):
         figures = []
-        for index, row in df_time_course.iterrows():
-            print(row["term"])
+        colors = ["#2ca02c", "#ff7f0e", "#1f77b4"]
+
+        # Define terms for grouping
+        different_terms = df_time_course.term.unique()
+        different_parties = df_time_course.party.unique()
+        different_media = df_time_course.media.unique()
+
+        for term in different_terms:
+            fig, axs = plt.subplots(1, 1)
+            fig.suptitle("Usage of term {0}".format(term))
+
+            for party in different_parties:
+                # plots to draw
+                df_step1 = df_time_course[df_time_course["term"] == term]
+                df_step2 = df_step1[df_step1["party"] == party]
+                #lines to draw
+                for media in different_media:
+                    df_plot = df_step2[df_step2["media"] == media]
+                    if not df_plot.empty:
+                        weights = df_plot["weight"]
+                        weights_array = np.asarray(weights)[0]
+                        axs.plot(range(len(weights_array)), weights_array)
+                        axs.set_title(media)
+
+            figures.append(fig)
         return figures
 
     @staticmethod
