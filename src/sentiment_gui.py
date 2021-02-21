94d8d04efdbc05178962c0e24df860df7baf7183
Kim-Celine Kahl <kim-celine.kahl@web.de>
2021-02-21 19:11:34 +0100

    add description for graphs

--- src/sentiment_gui.py
+++ src/sentiment_gui.py
@@ -1,13 +1,14 @@
+import datetime
 import tkinter
+import webbrowser
 
 import matplotlib.pyplot as plt
+import pandas as pd
+from dateutil.relativedelta import relativedelta
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
-import pandas as pd
 from keyword_extraction import KeywordExtraction
 from utils.visualization import Visualization
-import datetime
-from dateutil.relativedelta import relativedelta
 
 
 class SentimentGUI:
@@ -18,9 +19,11 @@ class SentimentGUI:
         self.plots = []
         self.current_plot = None
         self.current_plot_index = 0
+        self.current_help_message = ""
         self.gui = None
         self.next_button = None
         self.previous_button = None
+        self.help_button = None
         self.date_check = None
         self.cdu_check = None
         self.csu_check = None
@@ -42,16 +45,17 @@ class SentimentGUI:
 
     def configure_dataframe_for_time_course(self, start_date, end_date, media):
         df_paragraphs_time_interval = self.df_paragraphs_configured[self.df_paragraphs_configured["date"].notna()]
-        df_paragraphs_time_interval = df_paragraphs_time_interval[df_paragraphs_time_interval["media"]==media]
+        df_paragraphs_time_interval = df_paragraphs_time_interval[df_paragraphs_time_interval["media"] == media]
         if start_date and end_date:
             start = start_date.strftime("%Y-%m-%d")
             end = end_date.strftime("%Y-%m-%d")
             df_paragraphs_time_interval = df_paragraphs_time_interval[
                 (df_paragraphs_time_interval["date"] > start) & (df_paragraphs_time_interval["date"] < end)
-                ]
+            ]
         return df_paragraphs_time_interval
-    
+
     def clear_plots(self, clear_plot_array=False):
+        self.help_button["state"] = "disabled"
         if self.current_plot is not None:
             self.current_plot.get_tk_widget()["command"] = self.current_plot.get_tk_widget().grid_forget()
         if clear_plot_array:
@@ -74,6 +78,7 @@ class SentimentGUI:
         else:
             self.previous_button["state"] = "normal"
         self.clear_plots()
+        self.help_button["state"] = "normal"
         self.current_plot = self.plots[self.current_plot_index]
         self.current_plot.get_tk_widget().grid(row=4, column=0, columnspan=6)
 
@@ -106,6 +111,20 @@ class SentimentGUI:
     def show_sentiment(self, by_party):
         plt.close("all")
         self.clear_plots(clear_plot_array=True)
+        if by_party:
+            self.current_help_message = (
+                "These are piecharts showing the sentiment towards a certain party\n of the "
+                "selected media. This can be either positive, negative or neutral\n "
+                '(see legend). With a click on "Show next" or "Show previous"\n you can '
+                "see the sentiment for other parties."
+            )
+        else:
+            self.current_help_message = (
+                "These are piecharts showing the sentiment of a certain media\n towards the "
+                "selected parties. This can be either positive, negative or neutral\n "
+                '(see legend). With a click on "Show next" or "Show previous"\n you can '
+                "see the sentiment for other media."
+            )
         self.current_plot_index = 0
         party_list = self.get_parties()
         media_list = self.get_media()
@@ -123,6 +142,13 @@ class SentimentGUI:
         self.next_button["state"] = "disabled"
         self.previous_button["state"] = "disabled"
         self.clear_plots(clear_plot_array=True)
+        self.help_button["state"] = "normal"
+        self.current_help_message = (
+            "This is a bipartite graph showing the most important topics for the selected "
+            "parties.\n On the left side you can see the parties and on the right side the "
+            "the corresponding terms.\n The thickness of the connecting lines are the term "
+            "counts\n how often the term appears for each party."
+        )
         self.configure_dataframe()
         self.keyword_extraction.set_data(self.df_paragraphs_configured)
         party_list = self.get_parties()
@@ -137,13 +163,19 @@ class SentimentGUI:
         self.next_button["state"] = "normal"
         self.previous_button["state"] = "normal"
         self.clear_plots(clear_plot_array=True)
+        self.help_button["state"] = "normal"
+        self.current_help_message = (
+            "This is a line graph showing the importance of a certain term in the selected"
+            'media.\n With a click on "Show next" or "Show previous"\n'
+            "you can see the importance of another term."
+        )
         self.configure_dataframe()
         self.keyword_extraction.set_data(self.df_paragraphs_configured)
         party_list = self.get_parties()
         media_list = self.get_media()
         self.keyword_extraction.set_active_media(media_list)
         df_top_terms = self.keyword_extraction.get_top_terms_for_party(parties=party_list)
-        df_image = pd.DataFrame(columns=['party', 'media', 'term', 'weight'])
+        df_image = pd.DataFrame(columns=["party", "media", "term", "weight"])
         for party in party_list:
             for media in media_list:
                 # use only top 3 words for each party
@@ -159,17 +191,18 @@ class SentimentGUI:
                     dates = []
                     while next_end_date < initial_end_date:
                         # get weight for each month
-                        df_interval_paragraphs = self.configure_dataframe_for_time_course(start_date, next_end_date, media)
+                        df_interval_paragraphs = self.configure_dataframe_for_time_course(
+                            start_date, next_end_date, media
+                        )
                         weight = self.keyword_extraction.get_term_count(df_interval_paragraphs, party, term)
                         dates.append(start_date)
                         weight_list.append(weight)
                         start_date = next_end_date
                         next_end_date = start_date + relativedelta(months=+1)
-                    df_image = df_image.append({"party": party,
-                                                "media": media,
-                                                "term": term,
-                                                "weight": weight_list,
-                                                "dates": dates}, ignore_index=True)
+                    df_image = df_image.append(
+                        {"party": party, "media": media, "term": term, "weight": weight_list, "dates": dates},
+                        ignore_index=True,
+                    )
         # draw plot for time window
         figures = Visualization.get_plots(df_image)
         for fig in figures:
@@ -182,7 +215,7 @@ class SentimentGUI:
 
     def filter_time(self, min_date=None, max_date=None):
         self.df_paragraphs_configured = self.df_paragraphs_configured[self.df_paragraphs_configured["date"].notna()]
-        #self.df_paragraphs_configured["date"] = self.df_paragraphs_configured["date"].apply(lambda row: datetime.datetime.strptime(row, "%Y-%m-%d"))
+        # self.df_paragraphs_configured["date"] = self.df_paragraphs_configured["date"].apply(lambda row: datetime.datetime.strptime(row, "%Y-%m-%d"))
         print(self.df_paragraphs_configured.head())
         if min_date and max_date:
             self.df_paragraphs_configured = self.df_paragraphs_configured[
@@ -198,6 +231,19 @@ class SentimentGUI:
             self.df_paragraphs_configured = self.df_paragraphs
             self.entry_date_from.delete(0, "end")
             self.entry_date_to.delete(0, "end")
+
+    def popupmsg(self):
+        popup = tkinter.Tk()
+        popup.geometry("500x200")
+        popup.wm_title("Description")
+        label = tkinter.Label(popup, text=self.current_help_message)
+        label.pack(side="top", fill="x", pady=10)
+        button = tkinter.Button(popup, text="Okay", command=popup.destroy)
+        button.pack()
+        popup.mainloop()
+
+    def open_browser(self, url):
+        webbrowser.open_new(url)
 
     def show_gui(self):
         self.gui = tkinter.Tk()
@@ -273,17 +319,28 @@ class SentimentGUI:
         check_bild = tkinter.Checkbutton(self.gui, text="Bild", variable=self.bild_check, onvalue=1, offvalue=0)
         check_bild.grid(row=3, column=2)
 
-        self.next_button = button_by_outlet = tkinter.Button(
-            self.gui, text="Show next", command=lambda: self.show_diagram(increase=True)
-        )
+        self.next_button = tkinter.Button(self.gui, text="Show next", command=lambda: self.show_diagram(increase=True))
         self.next_button["state"] = "disabled"
-        button_by_outlet.grid(row=5, column=2)
+        self.next_button.grid(row=5, column=2)
 
-        self.previous_button = button_by_outlet = tkinter.Button(
+        self.previous_button = tkinter.Button(
             self.gui, text="Show previous", command=lambda: self.show_diagram(increase=False)
         )
         self.previous_button["state"] = "disabled"
-        button_by_outlet.grid(row=5, column=0)
+        self.previous_button.grid(row=5, column=0)
+
+        self.help_button = tkinter.Button(self.gui, text="What does this graph show?", command=lambda: self.popupmsg())
+        self.help_button["state"] = "disabled"
+        self.help_button.grid(row=5, column=4)
+
+        github_link = tkinter.Label(
+            self.gui,
+            text="Click here for more information about the project (GitHub repository)",
+            fg="blue",
+            cursor="hand2",
+        )
+        github_link.bind("<Button-1>", lambda e: self.open_browser("https://github.com/yakuhzi/news-analysis"))
+        github_link.grid(row=6, column=0, columnspan=6)
 
         # "Hack" for displaying topics correctly, otherwise they sometimes appear in pie charts
         self.show_topics()
